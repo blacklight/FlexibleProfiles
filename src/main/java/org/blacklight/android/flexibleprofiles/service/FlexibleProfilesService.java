4a28d3224ccf5d641acdc97e9653886d88708fbb
blacklight <blacklight86@gmail.com>
2013-10-07 14:38:35 +0200

    #3, #8

---
+++ src/main/java/org/blacklight/android/flexibleprofiles/service/FlexibleProfilesService.java
@@ -0,0 +1,19 @@
+package org.blacklight.android.flexibleprofiles.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class FlexibleProfilesService extends IntentService {
+
+	public FlexibleProfilesService(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onHandleIntent(Intent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
